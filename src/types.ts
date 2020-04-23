3a7f4dc45d54c861b56008d235e7a5a2f91a72aa
EmaSuriano <emanuel.suriano@gmail.com>
2020-04-23 16:31:19 +0200

    adding mapper basic logic

--- src/types.ts
+++ src/types.ts
@@ -52,12 +52,12 @@ type LanguageProficiency =
   | 'Limited working proficiency'
   | 'Native or bilingual proficiency';
 
-type Language = {
+type LinkedInLanguage = {
   name: string;
   proficiency: LanguageProficiency;
 };
 
-type Project = {
+type LinkedInProject = {
   name: string;
   date: string;
   description: string;
@@ -71,6 +71,125 @@ type LinkedInSchema = {
   educations: Education[];
   skills: Skill[];
   courses: Course[];
-  languages: Language[];
-  projects: Project[];
+  languages: LinkedInLanguage[];
+  projects: LinkedInProject[];
+};
+
+type JsonResumeProfile = {
+  network: string;
+  username: string;
+  url: string;
+};
+
+type JsonResumeBasics = {
+  name: string;
+  label: string;
+  image?: string;
+  email: string;
+  phone?: string;
+  url?: string;
+  summary: string;
+  location: {
+    address?: string;
+    postalCode?: string;
+    city?: string;
+    countryCode?: string;
+    region?: string;
+  };
+  profiles?: JsonResumeProfile[];
+};
+
+type JsonResumeWork = {
+  name: string;
+  location: string;
+  description: string;
+  position: string;
+  url?: string;
+  startDate: string;
+  endDate?: string;
+  summary: string;
+  highlights?: string[];
+};
+
+type JsonResumeVolunteer = {
+  organization: string;
+  position?: string;
+  url: string;
+  startDate: string;
+  endDate?: string;
+  summary: string;
+  highlights?: string[];
+};
+
+type JsonResumeEducation = {
+  institution: string;
+  area?: string;
+  studyType: string;
+  startDate: string;
+  endDate?: string;
+  gpa?: string;
+  courses?: string[];
+};
+
+type JsonResumeAward = {
+  title: string;
+  date: string;
+  awarder?: string;
+  summary: string;
+};
+
+type JsonResumePublication = {
+  name: string;
+  publisher?: string;
+  releaseDate: string;
+  url: string;
+  summary: string;
+};
+
+type JsonResumeSkill = {
+  name: string;
+  level?: string;
+  keywords?: string[];
+};
+
+type JsonResumeLanguage = {
+  language: string;
+  fluency: string;
+};
+
+type JsonResumeInterest = {
+  name: string;
+  keywords?: string[];
+};
+
+type JsonResumeReference = {
+  name: string;
+  reference: string;
+};
+
+type JsonResumeProject = {
+  name: string;
+  description: string;
+  highlights: string[];
+  keywords?: string[];
+  startDate?: string;
+  endDate?: string;
+  url: string;
+  roles: string[];
+  entity: string;
+  type: string;
+};
+
+type JsonResumeSchema = {
+  basics: JsonResumeBasics;
+  work: JsonResumeWork[];
+  volunteer?: JsonResumeVolunteer[];
+  education: JsonResumeEducation[];
+  awards?: JsonResumeAward[];
+  publications?: JsonResumePublication[];
+  skills?: JsonResumeSkill[];
+  languages?: JsonResumeLanguage[];
+  interests?: JsonResumeInterest[];
+  references?: JsonResumeReference[];
+  projects?: JsonResumeProject[];
 };
--- src/utils/mapLinkedInToJSONResume.ts
+++ src/utils/mapLinkedInToJSONResume.ts
@@ -1,8 +1,57 @@
-import { ResumeSchema } from '@kurone-kito/jsonresume-types';
+const mapToBasics = (linkedIn: LinkedInSchema): JsonResumeBasics => {
+  return {
+    name: 'string;',
+    label: 'string;',
+    image: 'string;',
+    email: 'string;',
+    phone: 'string;',
+    url: 'string;',
+    summary: 'string;',
+    location: {
+      address: 'string;',
+      postalCode: 'string;',
+      city: 'string;',
+      countryCode: 'string;',
+      region: 'string;',
+    },
+  };
+};
 
-const mapLinkedInToJSONResume = (linkedIn: LinkedInSchema): ResumeSchema => {
-  const resume = {
-    basics: {},
+const mapToWork = (linkedIn: LinkedInSchema): JsonResumeWork[] => {
+  return [
+    {
+      name: 'string;',
+      location: 'string;',
+      description: 'string;',
+      position: 'string;',
+      url: 'string;',
+      startDate: 'string;',
+      endDate: 'string;',
+      summary: 'string;',
+      highlights: ['string[];'],
+    },
+  ];
+};
+
+const mapToEducation = (linkedIn: LinkedInSchema): JsonResumeEducation[] => {
+  return [
+    {
+      institution: 'string',
+      area: 'string',
+      studyType: 'string',
+      startDate: 'string',
+      endDate: 'string',
+      gpa: 'string',
+      courses: ['string'],
+    },
+  ];
+};
+
+const mapLinkedInToJSONResume = (linkedIn: LinkedInSchema) => {
+  const resume: JsonResumeSchema = {
+    basics: mapToBasics(linkedIn),
+    work: mapToWork(linkedIn),
+    education: mapToEducation(linkedIn),
   };
 
   return resume;
