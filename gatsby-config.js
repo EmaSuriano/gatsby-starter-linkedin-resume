dde7b6bf4f4352df53e4603679488018649115fe
EmaSuriano <emanuel.suriano@gmail.com>
2020-05-31 01:22:35 +0200

    adding support for external resume source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
-const { existsSync } = require('fs');
+const { existsSync, writeFileSync } = require('fs');
 const { default: validate } = require('./lib/types/JsonResumeSchema.validator');
 const { RESUME_PATH } = require('./lib/utils/path');
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+const { RESUME_JSON } = process.env;
+
+if (RESUME_JSON) {
+  console.log('Downloading the resume from the external resource');
+  fetch(RESUME_JSON)
+    .then((x) => x.json())
+    .then((json) => writeFileSync(RESUME_PATH, JSON.stringify(json, null, 2)));
+}
 
 if (!existsSync(RESUME_PATH)) {
   throw new Error(
--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,15 @@ import mapLinkedInToJSONResume from './utils/mapLinkedInToJSONResume';
 import inquireLoginData from './utils/inquireLoginData';
 import getLinkedInData from './utils/getLinkedInData';
 import validateLinkedInSchema from './types/LinkedInSchema.validator';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 type CLIParams = {
   renew?: boolean;
 };
 
-const getCredentials = async () => {
-  const { EMAIL, PASS } = process.env;
-  if (EMAIL && PASS) {
-    log('Using logging credentials from environment values.');
-    return {
-      email: EMAIL,
-      password: PASS,
-    };
-  }
-
-  log('Please provide your LinkedIn credentials:');
-  return await inquireLoginData();
-};
-
 const main = async ({ renew }: CLIParams) => {
   if (renew || !fs.existsSync(LINKED_IN_PATH)) {
-    const credentials = await getCredentials();
+    log('Please provide your LinkedIn credentials:');
+    const credentials = await inquireLoginData();
     const profile = await getLinkedInData(credentials);
     fs.writeFileSync(LINKED_IN_PATH, JSON.stringify(profile, null, 2));
     log('LinkedIn Information saved!', LINKED_IN_PATH);
